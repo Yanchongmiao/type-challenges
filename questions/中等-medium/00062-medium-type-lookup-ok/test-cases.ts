a40659430acf5d17858205c59552e9d7d80c9145
yc6 <yanc0904@163.com>
2023-06-29 15:14:00 +0800

    medium: lookup

---
+++ questions/中等-medium/00062-medium-type-lookup-ok/test-cases.ts
@@ -0,0 +1,19 @@
+import type { Equal, Expect } from "@type-challenges/utils";
+type LookUp<T, U> = T extends { type: U } ? T : never;
+interface Cat {
+  type: "cat";
+  breeds: "Abyssinian" | "Shorthair" | "Curl" | "Bengal";
+}
+
+interface Dog {
+  type: "dog";
+  breeds: "Hound" | "Brittany" | "Bulldog" | "Boxer";
+  color: "brown" | "white" | "black";
+}
+
+type Animal = Cat | Dog;
+
+type cases = [
+  Expect<Equal<LookUp<Animal, "dog">, Dog>>,
+  Expect<Equal<LookUp<Animal, "cat">, Cat>>
+];
