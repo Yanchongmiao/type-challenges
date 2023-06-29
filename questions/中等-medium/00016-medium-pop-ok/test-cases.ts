72dc1db2616de78eb6ade2bd8d5963340cd5e95e
yc6 <yanc0904@163.com>
2023-06-29 14:02:48 +0800

    medium: pop array

---
+++ questions/中等-medium/00016-medium-pop-ok/test-cases.ts
@@ -0,0 +1,7 @@
+import type { Equal, Expect } from "@type-challenges/utils";
+type Pop<T extends any[]> = T extends [...infer O, infer P] ? O : [];
+type cases = [
+  Expect<Equal<Pop<[3, 2, 1]>, [3, 2]>>,
+  Expect<Equal<Pop<["a", "b", "c", "d"]>, ["a", "b", "c"]>>,
+  Expect<Equal<Pop<[]>, []>>
+];
