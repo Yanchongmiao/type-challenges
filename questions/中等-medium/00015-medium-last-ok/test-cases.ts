05ce69270e02269558064175028304e3abbb0773
yc6 <yanc0904@163.com>
2023-06-29 14:01:00 +0800

    medium: get array last

---
+++ questions/中等-medium/00015-medium-last-ok/test-cases.ts
@@ -0,0 +1,7 @@
+import type { Equal, Expect } from "@type-challenges/utils";
+type Last<T extends any[]> = T extends [...infer res, infer V] ? V : never;
+type cases = [
+  Expect<Equal<Last<[2]>, 2>>,
+  Expect<Equal<Last<[3, 2, 1]>, 1>>,
+  Expect<Equal<Last<[() => 123, { a: string }]>, { a: string }>>
+];
