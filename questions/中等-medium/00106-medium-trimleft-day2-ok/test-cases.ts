f99c2e252058ad1f74a6735b4c89770a5fe5a100
yc6 <yanc0904@163.com>
2023-06-30 09:30:36 +0800

    medium: timeleft

--- questions/中等-medium/00106-medium-trimleft-day2-ok/test-cases.ts
+++ questions/中等-medium/00106-medium-trimleft-day2-ok/test-cases.ts
@@ -1,5 +1,8 @@
 import type { Equal, Expect } from "@type-challenges/utils";
-
+type Space = " " | "\n" | "\t";
+type TrimLeft<T extends string> = T extends `${Space}${infer R}`
+  ? TrimLeft<R>
+  : T;
 type cases = [
   Expect<Equal<TrimLeft<"str">, "str">>,
   Expect<Equal<TrimLeft<" str">, "str">>,
