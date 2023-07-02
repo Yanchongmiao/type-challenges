1ae6e3cc48f333c62b27577e9ea0e60e87ec3b08
yc6 <yanc0904@163.com>
2023-07-02 16:43:05 +0800

    medium: day4

--- questions/中等-medium/01097-medium-isunion/test-cases.ts
+++ questions/中等-medium/01097-medium-isunion/test-cases.ts
@@ -1,10 +1,16 @@
-import type { Equal, Expect } from '@type-challenges/utils'
-
+import type { Equal, Expect } from "@type-challenges/utils";
+type IsUnion<T, T1 = T> = [T] extends [never]
+  ? false
+  : T extends any
+  ? [T1] extends [T]
+    ? false
+    : true
+  : never;
 type cases = [
   Expect<Equal<IsUnion<string>, false>>,
   Expect<Equal<IsUnion<string | number>, true>>,
-  Expect<Equal<IsUnion<'a' | 'b' | 'c' | 'd'>, true>>,
-  Expect<Equal<IsUnion<undefined | null | void | ''>, true>>,
+  Expect<Equal<IsUnion<"a" | "b" | "c" | "d">, true>>,
+  Expect<Equal<IsUnion<undefined | null | void | "">, true>>,
   Expect<Equal<IsUnion<{ a: string } | { a: number }>, true>>,
   Expect<Equal<IsUnion<{ a: string | number }>, false>>,
   Expect<Equal<IsUnion<[string | number]>, false>>,
@@ -12,6 +18,6 @@ type cases = [
   Expect<Equal<IsUnion<string | never>, false>>,
   Expect<Equal<IsUnion<string | unknown>, false>>,
   Expect<Equal<IsUnion<string | any>, false>>,
-  Expect<Equal<IsUnion<string | 'a'>, false>>,
-  Expect<Equal<IsUnion<never>, false>>,
-]
+  Expect<Equal<IsUnion<string | "a">, false>>,
+  Expect<Equal<IsUnion<never>, false>>
+];
